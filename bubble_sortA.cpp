51980ce4b6c1315c1615871d9845611d48184aca
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-05 15:19:35 +0700

    mendeklarasikan variable golbal array a berukuran 20

---
+++ bubble_sortA.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; //deklarasi variable global array a dengan ukuran 20
+int n; //deklarasi variable global n menyimpan banyaknya elemen
