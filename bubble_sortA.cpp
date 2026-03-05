3a6ec44466a735e3adb11dae4a19a74fd8e35763
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-05 15:32:45 +0700

    memasukkan prosedur program

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -4,3 +4,19 @@ using namespace std;
 
 int arr[20]; //deklarasi variable global array a dengan ukuran 20
 int n;  //deklarasi variable global n menyimpan banyaknya elemen bilangan bulat
+
+void input()
+{
+    int d;
+    while (true)
+    {
+        cout <<"Masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n<=20)
+            break;
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+}
