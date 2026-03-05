9afad6da1211cd48e89532f65128666722720b4c
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-05 16:02:45 +0700

    displai void

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -20,3 +20,65 @@ void input()
         }
     }
 }
+        cout << endl;
+        cout << "=====================" << endl;
+        cout << "masukkan elemen array" <<
+        cout << "=====================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+    cout << "Data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+}
+void bubblesortarray()
+{
+    int pass= 1 
+    do
+    {
+      for (int j = 0; j < n - pass; j++)
+      { // step 2
+        if (arr[j] > arr [j + 1])
+        {
+            int temp = arr [j];
+            arr[j] = arr[j+1]
+            arr[j+1] = temp;
+        } 
+      }  
+      pass += 1;
+
+      cout << "\npass " << pass - 1 << ": "
+       for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; // menampilkan data pada number of pass
+        }   
+        cout << endl;
+    
+    } while (pass <= n - 1); // step 5
+}
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Elemen array yang telah tersusun: " << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << " ";
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+}
+      
+    
+    
